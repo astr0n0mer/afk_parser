7541fc0ba48c9cb82db5719152246f9e476edb3c
astr0n0mer <42691857+astr0n0mer@users.noreply.github.com>
2024-08-18 01:48:45 +0530

    config: adds setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='afk_parser',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        "parsedatetime",
+    ],
+    description='A parser for AFK durations',
+    author='Imran Khan',
+    url='https://github.com/astr0n0mer/afk_parser',
+)
